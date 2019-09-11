43c56d7129d1060f6af988605bd85f8c5b9688f6
Raghuveer <jraghuveer848@gmail.com>
2019-09-11 19:42:50 +0530

    added Environment to change logs via properties file

--- src/main/java/com/raghu/personal/config/EmployeeConfiguration.java
+++ src/main/java/com/raghu/personal/config/EmployeeConfiguration.java
@@ -1,12 +1,19 @@
 package com.raghu.personal.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 public class EmployeeConfiguration {
 
+	@Autowired
+	Environment env;
+	
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
