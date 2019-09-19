6ec1486305508c2480a4278021e773af36d3bc86
Raghuveer <jraghuveer848@gmail.com>
2019-09-19 23:21:18 +0530

    implemented datasource

---
+++ src/main/java/com/raghu/personal/util/EmployeeUtil.java
@@ -0,0 +1,29 @@
+package com.raghu.personal.util;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeUtil {
+	
+	@PostConstruct
+	public void oninit() {
+		
+	}
+	
+	@Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+	
+	
+}
