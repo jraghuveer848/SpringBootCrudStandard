5a5027caabe50a5d94dd42bd8c87eca836c68b23
Raghuveer <jraghuveer848@gmail.com>
2019-09-19 23:28:51 +0530

    added primary key

--- src/main/java/com/raghu/personal/dao/entity/KeyMaster.java
+++ src/main/java/com/raghu/personal/dao/entity/KeyMaster.java
@@ -2,17 +2,30 @@ package com.raghu.personal.dao.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "T_Key_Master")
 public class KeyMaster {
 
+	@Id
+	@Column(name = "id")
+	private String id;
+
 	@Column(name = "public_key")
 	private String publickey;
 
 	@Column(name = "private_key")
 	private String privatekey;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getPublickey() {
 		return publickey;
